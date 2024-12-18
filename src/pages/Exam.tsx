2d06ac7d44b79f76b3e12fd7aa549eec7450da18
carlosalberto <carlosalberto@imaginagroup.com>
2024-12-18 10:38:49 +0100

    Logica para mapear las preguntas con su respuesta correcta

--- src/pages/Exam.tsx
+++ src/pages/Exam.tsx
@@ -1,4 +1,4 @@
-import {Flex,Text, OrderedList, ListItem} from "@chakra-ui/react"
+import {Flex, Text, OrderedList, ListItem, Accordion, AccordionItem, AccordionButton, Box, AccordionIcon, AccordionPanel} from "@chakra-ui/react"
 import { useContext } from 'react';
 import { DataContext } from "../context/useContext";
 
@@ -6,22 +6,51 @@ import { DataContext } from "../context/useContext";
 const Exam = () => {
 
     const {data} = useContext(DataContext);
+
+    const obtenerRespuesta = (preguntaId: number) => {
+        return data.respuestas.find(respuesta => respuesta.id === preguntaId);
+    };
+
     return (
-        <>
+        <Flex flexDirection="column" w="100vw" h="100vh" alignItems="center" justifyContent="center">
             <Text mb="20px" fontSize="28px" fontWeight="600">Examen</Text>
-            <Flex flexDirection="column" gap="60px">
-                {data.preguntas.map((pregunta) => (
-                    <Flex key={pregunta.id} flexDirection="column" gap="20px">
-                        <Text fontSize="16px" fontWeight="600">{pregunta.texto}</Text>
-                        <OrderedList>
-                            {pregunta.opciones.map((opcion, index) => (
-                                <ListItem key={index}>{opcion}</ListItem>
-                            ))}
-                        </OrderedList>
-                    </Flex>
-                ))}
-            </Flex>
-        </>
+            <Accordion allowToggle w="70vw">
+                {data.preguntas.map((pregunta) => {
+                    const respuesta = obtenerRespuesta(pregunta.id);
+                    return (
+                        <AccordionItem key={pregunta.id}>
+                            <h2>
+                                <AccordionButton>
+                                    <Box as='span' flex='1' textAlign='left'>
+                                        {pregunta.texto}
+                                    </Box>
+                                    <AccordionIcon />
+                                </AccordionButton>
+                            </h2>
+                            <AccordionPanel pb={4}>
+                                <OrderedList mb={4}>
+                                    {pregunta.opciones.map((opcion, index) => (
+                                        <ListItem 
+                                            key={index}
+                                            color={respuesta && index === respuesta.respuestaCorrecta ? "green.500" : "inherit"}
+                                            fontWeight={respuesta && index === respuesta.respuestaCorrecta ? "bold" : "normal"}
+                                        >
+                                            {opcion}
+                                        </ListItem>
+                                    ))}
+                                </OrderedList>
+                                {respuesta && (
+                                    <Box mt={2}>
+                                        <Text fontWeight="bold">Explicación:</Text>
+                                        <Text>{respuesta.explicacion}</Text>
+                                    </Box>
+                                )}
+                            </AccordionPanel>
+                        </AccordionItem>
+                    );
+                })}
+            </Accordion>
+        </Flex>
     )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { FormQuery } from "../components/FormQuery"
 
 const Home = () => {
     return (
-        <Flex w="100%" flexDirection="column">
+        <Flex w="100%" flexDirection="column" alignItems="center" justifyContent="center" width="100vw" height="100vh">
             <Text mb="20px" fontSize="28px" fontWeight="600">Home</Text>
             <FormQuery/>
         </Flex>
